615d7b21794f8883f5cf4c7532ef22632b657d3a
zayyyn <zbyszek791@hotmail.com>
2023-01-12 16:13:56 +0100

    feat: eslint and react

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,11 +1,11 @@
 module.exports = {
-    plugins: [
-        'prettier-plugin-astro',
-        'prettier-plugin-tailwindcss', // MUST come last
-    ],
-    pluginSearchDirs: false,
-    trailingComma: 'es5',
-    tabWidth: 4,
-    semi: true,
-    singleQuote: true,
+  plugins: [
+    'prettier-plugin-astro',
+    'prettier-plugin-tailwindcss', // MUST come last
+  ],
+  pluginSearchDirs: false,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
 };
