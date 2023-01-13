bfaaeb28725be0675aa2fb83bebf727a68ce8bf4
zayn <59846591+zbigniewstefaniuk@users.noreply.github.com>
2023-01-13 10:43:01 +0100

    feat(configuration)!: eslint and react integration (#1)
    * feat: eslint and react
    
    * chore: run prettier on files
    
    * chore(eslintrc): add rules and config for react
    
    * chore(eslintrc): add inline styles linter
    
    * chore(eslintrc): ts files linting
    
    * chore(eslint and setup); adjustments

---
+++ .eslintrc.cjs
@@ -0,0 +1,97 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:astro/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    'astro',
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'tailwindcss',
+  ],
+  rules: {
+    // ESLint: https://eslint.org/docs/latest/rules/
+    // TypeScript: https://typescript-eslint.io/rules/
+    // Astro: https://ota-meshi.github.io/eslint-plugin-astro/rules/
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.*'],
+      excludedFiles: ['*.astro'],
+      extends: ['plugin:react/recommended'],
+      rules: {
+        // React: https://github.com/jsx-eslint/eslint-plugin-react#list-of-supported-rules
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react/prop-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+      },
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
+    },
+    {
+      // Define the configuration for `.astro` file.
+      files: ['*.astro'],
+      plugins: ['astro'],
+      env: {
+        // Enables global variables available in Astro components.
+        node: true,
+        'astro/astro': true,
+        es2020: true,
+      },
+      // Allows Astro components to be parsed.
+      parser: 'astro-eslint-parser',
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+        sourceType: 'module',
+      },
+      rules: {
+        'astro/no-set-html-directive': 'warn',
+        'astro/no-unused-define-vars-in-style': 'warn',
+      },
+    },
+    {
+      // Define the configuration for `<script>` tag.
+      // Script in `<script>` is assigned a virtual file name with the `.js` extension.
+      files: ['**/*.astro/*.js', '*.astro/*.js'],
+      env: {
+        browser: true,
+        es2020: true,
+      },
+      parserOptions: {
+        sourceType: 'module',
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        'no-unused-vars': 'warn',
+      },
+    },
+  ],
+};
