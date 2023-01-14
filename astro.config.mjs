bcf18f0e3d6626b8aac568c646e039adff711e59
zayyyn <zbyszek791@hotmail.com>
2023-01-14 16:12:39 +0100

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-// https://astro.build/config
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,33 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Outfit', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: '#F9FAFF',
+        secondary: '#3B5EEA',
+        hover: '#506FEC',
+        disabled: '#8D9ACD',
+        red: '#EA3B65',
+        gray: {
+          light: '#F1F1F1',
+          DEFAULT: '#DBDBDB',
+          dark: '#89888A',
+        },
+      },
+      textColor: {
+        base: { DEFAULT: '#131115;', light: '#CFCFCF' },
+        primary: { DEFAULT: '#3B5EEA', light: '#8D9ACD' },
+      },
+    },
+    fontFamily: {
+      sans: ['Outfit', ...defaultTheme.fontFamily.sans],
+    },
   },
   plugins: [],
 };
