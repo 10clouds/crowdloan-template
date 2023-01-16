4b8f6e50562e000161bf2c3ded46e21a9b96ec8f
zayyyn <zbyszek791@hotmail.com>
2023-01-16 17:01:23 +0100

    feat: timeleft section

--- src/components/common/TimeLeftTimer.tsx
+++ src/components/common/TimeLeftTimer.tsx
@@ -2,27 +2,53 @@ import type { FC } from 'react';
 import { Fragment } from 'react';
 import { useTimeLeft } from '@/hooks';
 
-interface Props {
+export interface Props {
   date: string | Date;
+  variant?: TimerVariants;
 }
 
-const TimeLeftTimer: FC<Props> = ({ date }) => {
+type TimerVariants = keyof typeof mapVariants;
+
+const mapVariants = {
+  default: {
+    timer: 'text-[40px] md:text-6xl',
+    wrapper: 'w-16',
+    colon:
+      'text-[40px] leading-[14px] text-white md:text-6xl md:leading-[48px]',
+    gap: 'gap-2',
+  },
+  big: {
+    timer: 'text-[40px] md:text-[100px]',
+    wrapper: 'w-20',
+    colon:
+      'text-[40px] leading-[14px] text-white md:text-[100px] md:leading-[68px]',
+    gap: 'gap-4',
+  },
+} as const;
+
+const TimeLeftTimer: FC<Props> = ({ date, variant = 'default' }) => {
   const timeLeft = useTimeLeft(date);
 
   return (
-    <div className="mx-auto flex w-fit justify-center gap-4 rounded-t-xl bg-timer-gradient px-20 py-12 md:w-fit">
+    <div className={`flex h-fit ${mapVariants[variant].gap}`}>
       {Object.entries(timeLeft).map(([key, value], idx, arr) => (
         <Fragment key={key}>
-          <div className="flex w-16 flex-col items-center justify-center text-white">
-            <div className="text-[40px] font-medium md:text-6xl">
+          <div
+            className={`flex ${mapVariants[variant].wrapper} flex-col items-center justify-center text-white`}
+          >
+            <div
+              className={`text-[40px] font-medium md:text-6xl ${mapVariants[variant].timer}`}
+            >
               {Math.floor(Number(value))}
             </div>
-            <span className="text-sm">{key}</span>
+            <p className="text-sm">{key}</p>
           </div>
           {idx !== arr.length - 1 && (
-            <span className="text-[40px] font-medium leading-[14px] text-white md:text-6xl md:leading-[48px]">
+            <p
+              className={`font-medium  text-white ${mapVariants[variant].colon}`}
+            >
               :
-            </span>
+            </p>
           )}
         </Fragment>
       ))}
