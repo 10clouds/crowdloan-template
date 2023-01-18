d84b6feccc8bfdd6b75456c3a1cc1a7f4393efe0
zayn <59846591+zbigniewstefaniuk@users.noreply.github.com>
2023-01-18 19:42:54 +0100

    feat: PCFET-24 CTA Banner (#13)
    * feat: baner
    
    * chore: a bit of rwd

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -29,6 +29,7 @@ module.exports = {
         'timer-gradient':
           'linear-gradient(251.25deg, #436EE7 37.33%, #5043E7 87.85%);',
         'vector-1': 'url(/src/assets/backgrounds/Vector_1.svg)',
+        'vector-2': 'url(/src/assets/backgrounds/Vector_2.svg)',
       },
       backgroundColor: {
         base: { DEFAULT: '#3B5EEA', light: '#506FEC' },
