041a9cd280acae86960bfc02fc5c02ba7c5607c3
zayyyn <zbyszek791@hotmail.com>
2023-01-18 12:04:31 +0100

    feat: FAQ

---
+++ src/components/ui/Accordion/AccordionItem.tsx
@@ -0,0 +1,25 @@
+import { FC, useState } from 'react';
+
+interface Props {
+  answer: string;
+  question: string;
+}
+
+const AccordionItem: FC<Props> = ({ answer, question }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+  return (
+    <div className="w-fit max-w-fit px-8 py-6 ">
+      <span className={`text-3xl font-medium ${isExpanded && 'text-primary'}`}>
+        {question}
+      </span>
+      <button onClick={() => setIsExpanded((prev) => !prev)}>
+        {isExpanded ? '-' : '+'}
+      </button>
+      <div className={`${isExpanded ? 'h-auto' : 'h-0 overflow-hidden'}`}>
+        <p>{answer}</p>
+      </div>
+    </div>
+  );
+};
+
+export default AccordionItem;
