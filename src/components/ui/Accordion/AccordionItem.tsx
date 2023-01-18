ac9427566619cdd8c0e497c04752058379de9b97
zayyyn <zbyszek791@hotmail.com>
2023-01-18 12:31:20 +0100

    chore: separator

--- src/components/ui/Accordion/AccordionItem.tsx
+++ src/components/ui/Accordion/AccordionItem.tsx
@@ -8,8 +8,12 @@ interface Props {
 const AccordionItem: FC<Props> = ({ answer, question }) => {
   const [isExpanded, setIsExpanded] = useState(false);
   return (
-    <div className="w-fit max-w-fit px-8 py-6 ">
-      <span className={`text-3xl font-medium ${isExpanded && 'text-primary'}`}>
+    <div className="w-fit max-w-fit border-b px-8 py-6">
+      <span
+        className={`text-3xl font-medium ${
+          isExpanded && 'text-primary'
+        } transition-all duration-300 ease-in-out`}
+      >
         {question}
       </span>
       <button onClick={() => setIsExpanded((prev) => !prev)}>
