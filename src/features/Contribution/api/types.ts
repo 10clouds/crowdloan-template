6ae0da8fa6bbbe818f942e751eed918a5dece0a3
Michał Mrozowski <emero@users.noreply.github.com>
2023-02-01 22:56:30 +0100

    Update types.ts

--- src/features/Contribution/api/types.ts
+++ src/features/Contribution/api/types.ts
@@ -2,7 +2,7 @@ import type { Codec } from '@polkadot/types/types';
 import type { AccountData } from '@polkadot/types/interfaces/balances';
 import type { Index } from '@polkadot/types/interfaces/runtime';
 
-// ts have problem regarding this polkadot types and this is how they are defined in source code
+// ts has problems regarding this polkadot types and this is how they are defined in source code
 export type BalanceExtracted =
   | {
       now: Codec;
