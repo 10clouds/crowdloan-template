966e74987a1f59a09c6ea310bff72672b5e96c7f
zayyyn <zbyszek791@hotmail.com>
2023-02-22 17:29:20 +0100

    chore(FinalForm): added maax width

--- src/features/Contribution/components/FinalState.tsx
+++ src/features/Contribution/components/FinalState.tsx
@@ -14,7 +14,7 @@ const FinalState = ({ title, description, isError = false }: Props) => {
   const { setIsModalOpen } = useIsModalVisible();
 
   return (
-    <div className="flex h-full w-full min-w-[30vw] flex-col items-center justify-center gap-2 p-10">
+    <div className="flex h-full w-full min-w-[30vw] max-w-4xl flex-col items-center justify-center gap-2 p-10">
       <div>{isError ? <Close /> : <Check />}</div>
       <div className="text-4xl font-medium tracking-tight">{title}</div>
       <div className={`text-center text-lg ${isError ? 'text-error' : ''}`}>
